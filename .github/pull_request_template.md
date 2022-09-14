451bf80a32003e1b075fadda21cae0dd97655ece
Cory Ginsberg <cory.ginsberg1@gmail.com>
2022-09-14 10:34:17 -0500

    nits

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -1,8 +1,17 @@
 # Summary
-
-
-
-# Test Plan
+[Quick summary of everything you have done in this diff]
 
 ---
+# Test Plan
+
+### Desktop
+n/a
+
+### Mobile
+n/a
+
+### API
+n/a
+
 ## Regression Test
+[Confirm old stuff still works]
--- pages/helper/weatherDataHelper.ts
+++ pages/helper/weatherDataHelper.ts
@@ -1,53 +1,49 @@
 enum PressureTrend {
-    RISING = 'rising',
-    FALLING = 'falling',
-    STEADY = 'steady',
-};
+  RISING = "rising",
+  FALLING = "falling",
+  STEADY = "steady",
+}
 
 interface RequestMetadata {
-    attributionURL: string,
-    expireTime: string,
-    latitude: number,
-    longitude: number,
-    readTime: string,
-    reportedTime: string,
-    units: string,
-    version: number,
+  attributionURL: string;
+  expireTime: string;
+  latitude: number;
+  longitude: number;
+  readTime: string;
+  reportedTime: string;
+  units: string;
+  version: number;
 }
 
 export interface CurrentWeather {
-    name: string,
-    metadata: RequestMetadata,
-    asOf: Date,
-    cloudCover: number,
-    conditionCode: string,
-    daylight: boolean,
-    humidity: number,
-    precipitationIntensity: number,
-    pressure: number,
-    pressureTrend: PressureTrend,
-    temperature: number,
-    temperatureApparent: number,
-    temperatureDewPoint: number,
-    uvIndex: number,
-    visibility: number,
-    windDirection: number,
-    windGust: number,
-    windSpeed: number,
+  name: string;
+  metadata: RequestMetadata;
+  asOf: Date;
+  cloudCover: number;
+  conditionCode: string;
+  daylight: boolean;
+  humidity: number;
+  precipitationIntensity: number;
+  pressure: number;
+  pressureTrend: PressureTrend;
+  temperature: number;
+  temperatureApparent: number;
+  temperatureDewPoint: number;
+  uvIndex: number;
+  visibility: number;
+  windDirection: number;
+  windGust: number;
+  windSpeed: number;
 }
 
 // https://developer.apple.com/documentation/weatherkitrestapi/dailyforecast
-export interface DailyForecast {
-
-}
+export interface DailyForecast {}
 
 // https://developer.apple.com/documentation/weatherkitrestapi/daypartforecast
-export interface DayPartForecast {
-
-}
+export interface DayPartForecast {}
 
 // https://developer.apple.com/documentation/weatherkitrestapi/dayweatherconditions
 export interface DayWeatherConditions {
-    conditionCode: string,
-    daytimeForecase: DayPartForecast,
+  conditionCode: string;
+  daytimeForecase: DayPartForecast;
 }
